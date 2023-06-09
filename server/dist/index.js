baaa1cb789ffca34fa1723715e02a49adcdd9e4b
Shrenik Deep <s.shrenikdeep@gmail.com>
2023-06-09 12:22:09 +0530

    Backend Updates

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,6 +1,17 @@
-import express from "express";
-const app = express();
-app.listen(5000, () => {
-    console.log("Running on port 5000!");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const port = 8000;
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.send("HELLO FROM EXPRESS + TS!!!!");
 });
-//# sourceMappingURL=index.js.map
+app.get("/hi", (req, res) => {
+    res.send("BYEE!!!");
+});
+app.listen(port, () => {
+    console.log(`now listening on port ${port}`);
+});
